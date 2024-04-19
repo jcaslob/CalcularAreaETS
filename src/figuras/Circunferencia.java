22e826deb8dae3501bfe7e22ca74c688f90d6efe
jcaslob <javier_c.l@hotmail.es>
2024-04-19 15:35:32 +0100

    Se añaden comentarios javadoc y también se añade la documentación javadoc

--- src/figuras/Circunferencia.java
+++ src/figuras/Circunferencia.java
@@ -1,41 +1,87 @@
 package figuras;
 
+/**
+ * Clase que representa una circunferencia con propiedades y métodos
+ * relacionados.
+ * 
+ * @author Javier Castro Lobato
+ * @version 19/04/2024
+ */
 public class Circunferencia {
+	/**
+	 * Radio de la circunferencia.
+	 */
 	private double radio;
+
+	/**
+	 * Color de la circunferencia.
+	 */
 	private String color;
 
+	/**
+	 * Constructor que inicializa una instancia de la circunferencia con un radio
+	 * dado.
+	 * 
+	 * @param radio El radio de la circunferencia.
+	 */
 	public Circunferencia(double radio) {
 		this.radio = radio;
 	}
 
+	/**
+	 * Imprime información sobre la circunferencia, incluyendo diámetro, color y
+	 * área.
+	 */
 	public void imprimir() {
+		// Asigna el color rojo a la circunferencia
 		color = "rojo";
-		System.out.println("Di�metro: " + (2 * radio));
+		// Imprime el diámetro de la circunferencia
+		System.out.println("Diámetro: " + (2 * radio));
+		// Imprime el color de la circunferencia
 		System.out.println("Color: " + color);
+		// Calcula y muestra el área de la circunferencia
 		double area = 2 * 3.1416 * radio * radio;
-		System.out.println(area);
+		System.out.println("Área: " + area);
 	}
 
+	/**
+	 * Compara si esta circunferencia es igual a otra, considerando o no los
+	 * decimales.
+	 * 
+	 * @param considerarDecimales Si se deben considerar los decimales en la
+	 *                            comparación.
+	 * @param otro                Otra circunferencia con la que se compara.
+	 * @return Verdadero si las circunferencias son iguales, falso en caso
+	 *         contrario.
+	 * @throws IllegalArgumentException Si el objeto 'otro' es nulo.
+	 */
 	public boolean esIgual(boolean considerarDecimales, Circunferencia otro) {
+		if (otro == null) {
+			throw new IllegalArgumentException("El objeto 'otro' no puede ser nulo.");
+		}
 		double radio1 = this.radio;
 		double radio2 = otro.getRad();
 		if (considerarDecimales) {
-			if (radio1 == radio2)
-				return true;
-			else
-				return false;
+			return radio1 == radio2;
 		} else {
-			if (Math.abs(radio1 - radio2) < 1)
-				return true;
-			else
-				return false;
+			return Math.abs(radio1 - radio2) < 1;
 		}
 	}
 
+	/**
+	 * Obtiene el radio de la circunferencia.
+	 * 
+	 * @return El radio de la circunferencia.
+	 */
 	public double getRad() {
 		return radio;
 	}
 
+	/**
+	 * Establece un nuevo valor para el radio de la circunferencia.
+	 * 
+	 * @param rad El nuevo valor del radio.
+	 */
 	public void setRad(double rad) {
 		this.radio = rad;
 	}
--- src/figuras/Test.java
+++ src/figuras/Test.java
@@ -1,16 +1,31 @@
 package figuras;
 
+/**
+ * Clase Test que contiene el método main para probar la clase Circunferencia.
+ * 
+ * @author Javier Castro Lobato
+ * @version 19/04/2024
+ */
 public class Test {
+	/**
+	 * Método principal que se ejecuta al iniciar el programa.
+	 * 
+	 * @param args Argumentos de la línea de comandos.
+	 */
 	public static void main(String[] args) {
+		// Creación de una circunferencia con radio 5.5
 		Circunferencia c1 = new Circunferencia(5.5);
+		// Creación de una circunferencia con radio 10.1
 		Circunferencia c2 = new Circunferencia(10.1);
+		// Creación de una circunferencia con radio 10.9
 		Circunferencia c3 = new Circunferencia(10.9);
 
+		// Compara c2 y c3 sin considerar los decimales
 		if (c2.esIgual(false, c3))
 			System.out.println("c2 y c3: iguales sin considerar decimales");
 
+		// Compara c2 y c3 considerando los decimales
 		if (c2.esIgual(true, c3))
 			System.out.println("c2 y c3: iguales considerando decimales");
 	}
-
 }
